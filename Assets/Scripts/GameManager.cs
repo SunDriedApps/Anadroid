c4db72dc00c96e9cac3eea66e957260636bb4af7
Adam <sundriedapps@gmail.com>
2016-02-22 15:02:30 +0000

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi.Multiplayer;
+using System;
+
+public class GameManager : RealTimeMultiplayerListener
+{
+    public void OnLeftRoom()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnParticipantLeft(Participant participant)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPeersConnected(string[] participantIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPeersDisconnected(string[] participantIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnRealTimeMessageReceived(bool isReliable, string senderId, byte[] data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnRoomConnected(bool success)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnRoomSetupProgress(float percent)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/SetupPlayGamesServices.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms;
+
+public class SetupPlayGamesServices : MonoBehaviour
+{
+
+    void Start()
+    {
+        // Select the Google Play Games platform as our social platform implementation
+        GooglePlayGames.PlayGamesPlatform.Activate();
+
+        Social.localUser.Authenticate((bool success) => {
+
+            if (success)
+            {
+
+                string token = GooglePlayGames.PlayGamesPlatform.Instance.GetToken();
+                Debug.Log(token);
+            }
+            else {
+
+            }
+        });
+    }
+}
