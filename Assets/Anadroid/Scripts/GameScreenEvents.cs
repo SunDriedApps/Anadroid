d966058e922aacccb77178a70cd7e38b069f2c60
cormacsugrue <cormac.sugrue4@mail.dcu.ie>
2016-03-03 16:31:01 +0000

    Added Ui funtions

--- Assets/Anadroid/Scripts/GameScreenEvents.cs
+++ Assets/Anadroid/Scripts/GameScreenEvents.cs
@@ -4,8 +4,45 @@ using UnityEngine.UI;
 
 public class GameScreenEvents : MonoBehaviour {
 
+    private bool checkButtonOn = false;
+    private bool buttonMoveFin = false;
+    GameObject buttonToMove = null;
+   // GameObject nextBlank = null;
+
+    /*public void getNextBlank()
+    {
+        nextBlank
+    }*/
+
+    // this method is called when by on click button, the button supplies it's game object 
+    // boolean checkForButton is switched, buttonToMove is set
+    public void buttonPressed(GameObject buttonClicked)
+    {
+        checkButtonOn = true;
+        buttonMoveFin = false;
+        buttonToMove = buttonClicked;
+    }
+
     public void Update()
     {
+        if (checkButtonOn)
+        {
+              MoveButton(buttonToMove/*,nextBlankButton8*/);
+        }
         
+    }
+
+    public void MoveButton(GameObject button/*, GameObject buttonDest*/)
+    {
+        float speed = 3500f;
+
+        Vector3 newPos = new Vector3(150, 600, 0);
+      
+        float step = speed * Time.deltaTime;
+        button.transform.position = Vector3.MoveTowards(button.transform.position, newPos, step);
+
+        
+
+       
     }
 }
---
+++ Assets/Anadroid/Scripts/LetterSelect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LetterSelect : MonoBehaviour  {
+    
+	
+	public void MoveButton (GameObject button) {
+	    float speed = 10;
+
+        Vector3 newPos = new Vector3(0, 0, 0);
+
+        Vector3 currentPos = button.transform.position;
+
+        Vector3 directionOfMove = newPos - currentPos;
+
+        directionOfMove.Normalize();
+
+        button.transform.Translate(
+            (directionOfMove.x * speed * Time.deltaTime),
+            (directionOfMove.y * speed * Time.deltaTime),
+            (directionOfMove.z * speed * Time.deltaTime)
+            );
+
+	}
+}
